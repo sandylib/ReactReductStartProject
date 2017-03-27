f1627a133a2c4052b2e92dbab49ab92688921a06
Sandy <sandyz@bizcover.com.au>
2017-03-27 16:29:15 +1100

    Inital App Structrue

---
+++ src/components/course/CoursePage.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <p>This is the Courses page</p>
+       
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
